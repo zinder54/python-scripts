b2f180e76aed5a78a250f768818c03cd3c6432e9
zinder54 <zinder.ab@gmail.com>
2020-09-22 20:32:14 +0100

    n

--- Work/qt.py
+++ Work/qt.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtCore,QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
 from kiwaqt import Ui_MainWindow
 import sys
@@ -19,7 +19,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         SaveFile = QAction("&Save File", self)
         SaveFile.setShortcut("Ctrl+S")
         SaveFile.setStatusTip('Save File')
-        SaveFile.triggered.connect(self.file_open)
+        SaveFile.triggered.connect(self.save_file)
 
         self.statusBar()
 
@@ -29,18 +29,16 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         fileMenu.addAction(SaveFile)
 
 
-    def file_open():
-        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
-        file = open(name,'r')
-        self.editor()
-        with file:
-            text = file.read()
-            self.textEdit.setText(text)
-    def save_file():
-        #name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
-        with open('test.txt', 'w') as f:
-            text = self.textEdit.toPlainText()
-            f.write(text)
+    def file_open(self):
+        files, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()","","All Files (*);;python Files (*.py)")
+        if files:
+            print(files)
+    def save_file(self):
+        options = QFileDialog.Options()
+        name, _ = QFileDialog.getSaveFileName(self, "QFileDialog.getSaveFileName()","","Text Files (*.txt);;All Files (*)",
+                                            options=options)
+        if name:
+            print(name)
 
 
 
