e40d3fac128cfcf34cd1245ef35cd27c84677378
zinder54 <zinder.ab@gmail.com>
2020-10-07 20:50:09 +0100

    added frame

--- Work/structures.py
+++ Work/structures.py
@@ -4,93 +4,123 @@ import tkinter.ttk as ttk
 
 app = Tk()
 app.title("Kiwa site calculations")
-app.resizable(0, 0)
-app.geometry("750x300")
+app.resizable(1000, 1000)
+app.geometry("710x500")
 
-m1 = PanedWindow(orient = HORIZONTAL)
-m1.grid(row=0, column=0, sticky ="nsew")
+main_frame = Frame(app,width=500,height=50, borderwidth=5, relief='raised')
+second_frame = Frame(app,width=500,height=50, borderwidth=5)
+main_frame.grid(row=0, sticky ="w")
+second_frame.grid(row=1, sticky ="w")
 app.grid_rowconfigure(5, weight=5)
-app.grid_columnconfigure(5, weight=5)
-m2 = PanedWindow(m1, orient = HORIZONTAL)
+app.grid_columnconfigure(4, weight=5)
 
-scrollbar = Scrollbar(m1,  orient = HORIZONTAL, command=m1.yview)
-L1 = Label(m1, text="Client",
+L1 = Label(main_frame, text="Client",
            pady=15,
            padx=10).grid(row=0, column=1)
-E1 = Entry(m1).grid(row=1, column=1)
+E1 = Entry(main_frame).grid(row=1, column=1)
 
-L2 = Label(m1, text = "Site",
+L2 = Label(main_frame, text = "Site",
            pady=15,
            padx=30).grid(row=0, column=2)
-E2 = Entry(m1).grid(row=1, column=2)
+E2 = Entry(main_frame).grid(row=1, column=2)
 
-L3 = Label(m1, text="Location",
+L3 = Label(main_frame, text="Location",
            pady=15,
            padx=30).grid(row = 0, column =3)
-E3 = Entry(m1).grid(row=1, column=3)
+E3 = Entry(main_frame).grid(row=1, column=3)
 
-L4 = Label(m1, text="Job REF.",
+L4 = Label(main_frame, text="Job REF.",
            pady=15,
            padx=30).grid(row=0, column=4)
-E4 = Entry(m1).grid(row=1, column=4)
+E4 = Entry(main_frame).grid(row=1, column=4)
 
-L5 = Label(m1, text="DATE",
+L5 = Label(main_frame, text="DATE",
            pady=15,
            padx=30).grid(row=0, column=5)
-E5 = Entry(m1).grid(row=1, column=5)
+E5 = Entry(main_frame).grid(row=1, column=5)
 
-L6 = Label(m1, text="Length under Test",
+L6 = Label(main_frame, text="Length under Test",
            pady=5,
            padx=0).grid(row=2,column=4)
-E6 = Entry(m1).grid(row=2, column=5)
+E6 = Entry(main_frame).grid(row=2, column=5)
 
-L7 = Label(m1, text="Spec. load/m(Bedding)",
+L7 = Label(main_frame, text="Spec. load/m(Bedding)",
            pady=5,
            padx=0).grid(row=2,column=3)
 
-L7a = Label(m1, text="Kn/m",
+L7a = Label(main_frame, text="Kn/m",
            pady=0,
            padx=0).grid(row=3,column=2)
-E7a = Entry(m1).grid(row=3, column=3)
+E7a = Entry(main_frame).grid(row=3, column=3)
 
-L7b = Label(m1, text="/m",
+L7b = Label(main_frame, text="/m",
            pady=0,
            padx=0).grid(row=4,column=2)
-E7b = Entry(m1).grid(row=4, column=3)
+E7b = Entry(main_frame).grid(row=4, column=3)
 
-L8 = Label(m1, text="Spec. load/m(Proof)",
+L8 = Label(main_frame, text="Spec. load/m(Proof)",
             pady=0,
             padx=0).grid(row=2, column=1)
-L8a = Label(m1, text="Kn/m",
+L8a = Label(main_frame, text="Kn/m",
             pady=0,
             padx=0).grid(row=3, column=0)
-E8a = Entry(m1).grid(row=3, column=1)
-L8b = Label(m1, text="/m",
+E8a = Entry(main_frame).grid(row=3, column=1)
+L8b = Label(main_frame, text="/m",
             pady=0,
             padx=0).grid(row=4, column=0)
-E8b = Entry(m1).grid(row=4, column=1)
+E8b = Entry(main_frame).grid(row=4, column=1)
 
-L9 = Label(m1,text="Load at X(bedding)",
+L9 = Label(main_frame,text="Load at X(bedding)",
             pady=0,
             padx=0,).grid(row=3, column=4)
-E9 = Entry(m1).grid(row=3,column=5)
+E9 = Entry(main_frame).grid(row=3,column=5)
 
-L10 = Label(m1,text="Load at X(Proof)",
+L10 = Label(main_frame,text="Load at X(Proof)",
             pady=0,
             padx=0,).grid(row=4, column=4)
-E10 = Entry(m1).grid(row=4,column=5)
+E10 = Entry(main_frame).grid(row=4,column=5)
 
-LB1 = Label(m1, text="barrier,Span/",
+L11 = Label(main_frame, text="barrier, Span/ ",
             pady=0,
             padx=0).grid(row=5,column=0)
-EB1 = Entry(m1).grid(row=5,column=1)
-EB2 = Entry(m1).grid(row=5,column=2)
-EB3 = Entry(m1).grid(row=5,column=3)
-EB4 = Entry(m1).grid(row=5,column=4)
-EB5 = Entry(m1).grid(row=5,column=5)
+E11 = Entry(main_frame).grid(row=5,column=1)
+E12 = Entry(main_frame).grid(row=5,column=2)
+E13 = Entry(main_frame).grid(row=5,column=3)
+E14 = Entry(main_frame).grid(row=5,column=4)
+E15 = Entry(main_frame).grid(row=5,column=5)
 
-LB2 = Label(m1, text="Bedding load",
-            pady=0, padx=0).grid(row=6,column=0)
+sfL1 = Label(second_frame, text="Bedding cycle",
+            pady=0, padx=0).grid(row=0,column=0)
+sfL2 = Label(second_frame, text="MAX DEF.",
+            pady=0, padx=0).grid(row=1,column=0)
+sfL3 = Label(second_frame, text="RESIDUAL DEF.",
+            pady=0, padx=0).grid(row=2,column=0)
+sfE1 = Entry(second_frame).grid(row=0,column=1)
+sfE2 = Entry(second_frame).grid(row=0,column=2)
+sfE3 = Entry(second_frame).grid(row=0,column=3)
+sfE4 = Entry(second_frame).grid(row=0,column=4)
+sfE5 = Entry(second_frame).grid(row=0,column=5)
+sfE6 = Entry(second_frame).grid(row=1,column=1)
+sfE7 = Entry(second_frame).grid(row=1,column=2)
+sfE8 = Entry(second_frame).grid(row=1,column=3)
+sfE9 = Entry(second_frame).grid(row=1,column=4)
+sfE10 = Entry(second_frame).grid(row=1,column=5)
+sfE11 = Entry(second_frame).grid(row=2,column=1)
+sfE12 = Entry(second_frame).grid(row=2,column=2)
+sfE13 = Entry(second_frame).grid(row=2,column=3)
+sfE14 = Entry(second_frame).grid(row=2,column=4)
+sfE15 = Entry(second_frame).grid(row=2,column=5)
+
+sfL4 = Label(second_frame, text="Proof cycle",
+            pady=5, padx=0).grid(row=3,column=0)
+sfL5 = Label(second_frame, text="MAX DEF.",
+            pady=0, padx=0).grid(row=4,column=0)
+sfL6 = Label(second_frame, text="RESIDUAL DEF.",
+            pady=0, padx=0).grid(row=5,column=0)
+sfL7 = Label(second_frame, text="RESIDUAL DEF.",
+            pady=0, padx=0).grid(row=5,column=0)
+sfL8 = Label(second_frame, text="RESIDUAL DEF.",
+            pady=0, padx=0).grid(row=5,column=0)
 
 #height  = 10
 #width = 10
