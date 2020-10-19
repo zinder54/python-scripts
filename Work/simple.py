ef83789d946f4927a02988ebfb86a3ba18802cca
zinder54 <zinder.ab@gmail.com>
2020-10-19 21:18:12 +0100

    updates

--- Work/simple.py
+++ Work/simple.py
@@ -1,35 +1,38 @@
-# import tkinter
-#import tkinter as tk
-#from tkinter import *
-#import tkinter.ttk as ttk
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-# create the gui app and size of the app
-#app = Tk()
-##app.title("kiwa barrier testing")
-#app.geometry('400x400')
+#create the gui app and size of the app
+app = Tk()
+nb = ttk.Notebook(app)
 
-#create a table
-try:
-    from tkinter import *
-except ImportError:
-    from Tkinter import *
+main_frame = Frame(nb) 
+bedding_frame = Frame(main_frame,width=500,height=50,borderwidth=5, relief='flat')
+proof1_frame = Frame(main_frame,width=500,height=50,borderwidth=5, relief='flat')
+proof2_frame = Frame(main_frame,width=500,height=50,borderwidth=5, relief='flat')
 
-root = Tk()
+bedding_frame.pack(side='top')
+proof1_frame.pack(side='bottom')
+proof2_frame.pack(side='bottom')
+nb.add(main_frame, text="main")
+nb.pack(pady=15)
+app.grid_rowconfigure(6, weight=6)
+app.grid_columnconfigure(6, weight=6)
 
-height = 5 + 1
-width = 5
+L1= Label(bedding_frame, text="Span").grid(row=0)
+L2 = Label(bedding_frame, text="Bedding Load").grid(row=1)
+
+height = 7 
+width = 6
 for i in range(height): #Rows
     for j in range(width): #Columns
-        b = Entry(root, text="")
-        b.grid(row=i, column=j)
+        if i == 2:
+            continue
+        bedding = Entry(bedding_frame)
+        proof1 = Entry(proof1_frame)
+        proof2 = Entry(proof2_frame)
+        bedding.grid(row=i, column=1+j)
+        proof1.grid(row=i, column=1+j)
+        proof2.grid(row=i, column=1+j)
 
-
-#l1 =
-
-
-
-
-mainloop()
-
-
-#app.mainloop()
+app.mainloop()
--- Work/structures.py
+++ Work/structures.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Button, Entry, Label, StringVar
 from tkinter import *
-import tkinter.ttk as ttk
+from tkinter import ttk
 
 app = Tk()
 app.title("Kiwa site calculations")
@@ -9,115 +9,110 @@ app.geometry("835x500")
 nb = ttk.Notebook(app)
 
 
-
-main_frame = ttk.Frame(nb,width=500,height=50,borderwidth=5, relief='raised')
-second_frame = ttk.Frame(nb,width=500,height=50, borderwidth=1)
-nb.add(main_frame, text="main")
-nb.add(second_frame, text="second")
-nb.select(main_frame)
-nb.grid(app,row=1)
-main_frame.grid(row=0, sticky ="w")
-second_frame.grid(row=1, sticky ="w")
+main = Frame(nb)
+header_frame = Frame(main,width=500,height=50,borderwidth=5, relief='raised')
+calc_frame = Frame(main,width=500,height=50, borderwidth=1)
+header_frame.pack(side='top')
+calc_frame.pack(side='bottom')
+nb.add(main, text="main")
+nb.pack(pady=15)
 app.grid_rowconfigure(6, weight=6)
 app.grid_columnconfigure(6, weight=6)
 
 
-L1 = Label(main_frame, text="Client",
+L1 = Label(header_frame, text="Client",
            pady=15,
            padx=10).grid(row=0, column=1)
 for i in range(5):
-    Entry(main_frame).grid(row=1, column=1+i)
-L2 = Label(main_frame, text = "Site",
+    Entry(header_frame).grid(row=1, column=1+i)
+L2 = Label(header_frame, text = "Site",
            pady=15,
            padx=30).grid(row=0, column=2)
 
-L3 = Label(main_frame, text="Location",
+L3 = Label(header_frame, text="Location",
            pady=15,
            padx=30).grid(row = 0, column =3)
 
-L4 = Label(main_frame, text="Job REF.",
+L4 = Label(header_frame, text="Job REF.",
            pady=15,
            padx=30).grid(row=0, column=4)
 
-L5 = Label(main_frame, text="DATE",
+L5 = Label(header_frame, text="DATE",
            pady=15,
            padx=30).grid(row=0, column=5)
 
-L6 = Label(main_frame, text="Length under Test",
+L6 = Label(header_frame, text="Length under Test",
            pady=5,
            padx=0).grid(row=2,column=4)
 
-L7 = Label(main_frame, text="Spec. load/m(Bedding)",
+L7 = Label(header_frame, text="Spec. load/m(Bedding)",
            pady=5,
            padx=0).grid(row=2,column=3)
 
-L7a = Label(main_frame, text="Kn/m",
+L7a = Label(header_frame, text="Kn/m",
            pady=0,
            padx=0).grid(row=3,column=2)
-E7a = Entry(main_frame).grid(row=3, column=3)
+E7a = Entry(header_frame).grid(row=3, column=3)
 
-L7b = Label(main_frame, text="/m",
+L7b = Label(header_frame, text="/m",
            pady=0,
            padx=0).grid(row=4,column=2)
-E7b = Entry(main_frame).grid(row=4, column=3)
+E7b = Entry(header_frame).grid(row=4, column=3)
 
-L8 = Label(main_frame, text="Spec. load/m(Proof)",
+L8 = Label(header_frame, text="Spec. load/m(Proof)",
             pady=0,
             padx=0).grid(row=2, column=1)
-L8a = Label(main_frame, text="Kn/m",
+L8a = Label(header_frame, text="Kn/m",
             pady=0,
             padx=0).grid(row=3, column=0)
-E8a = Entry(main_frame).grid(row=3, column=1)
-L8b = Label(main_frame, text="/m",
+E8a = Entry(header_frame).grid(row=3, column=1)
+L8b = Label(header_frame, text="/m",
             pady=0,
             padx=0).grid(row=4, column=0)
-E8b = Entry(main_frame).grid(row=4, column=1)
+E8b = Entry(header_frame).grid(row=4, column=1)
 
-L9 = Label(main_frame,text="Load at X(bedding)",
+L9 = Label(header_frame,text="Load at X(bedding)",
             pady=0,
             padx=0,).grid(row=3, column=4)
-E9 = Entry(main_frame).grid(row=3,column=5)
+E9 = Entry(header_frame).grid(row=3,column=5)
 
-L10 = Label(main_frame,text="Load at X(Proof)",
+L10 = Label(header_frame,text="Load at X(Proof)",
             pady=0,
             padx=0,).grid(row=4, column=4)
-E10 = Entry(main_frame).grid(row=4,column=5)
+E10 = Entry(header_frame).grid(row=4,column=5)
 
-L11 = Label(main_frame, text="barrier, Span/ ",
+L11 = Label(header_frame, text="barrier, Span/ ",
             pady=0,
             padx=0).grid(row=5,column=0)
 for i in range(6):
-    Entry(main_frame).grid(row=5, column=1+i)
+    Entry(header_frame).grid(row=5, column=1+i)
 
-sfL1 = Label(second_frame, text="Bedding cycle",
+sfL1 = Label(calc_frame, text="Bedding cycle",
             pady=0, padx=0).grid(row=0,column=0)
-sfL2 = Label(second_frame, text="MAX DEF.",
+sfL2 = Label(calc_frame, text="MAX DEF.",
             pady=0, padx=0).grid(row=1,column=0)
-sfL3 = Label(second_frame, text="RESIDUAL DEF.",
+sfL3 = Label(calc_frame, text="RESIDUAL DEF.",
             pady=0, padx=0).grid(row=2,column=0)
 #For loop for full line entry boxes in second Frame
-for i in range(6):
-    Entry(second_frame).grid(row=0, column=1+i)
-    Entry(second_frame).grid(row=1, column=1+i)
-    Entry(second_frame).grid(row=2, column=1+i)
-    Entry(second_frame).grid(row=4, column=1+i)
-    Entry(second_frame).grid(row=5, column=1+i)
-    Entry(second_frame).grid(row=6, column=1+i)
-    Entry(second_frame).grid(row=7, column=1+i)
+for i in range(6):#height of entry
+    for j in range(7):#width of entry
+        if j == 2: # skips row 2
+            continue
+        Entry(calc_frame).grid(row=1+j, column=1+i)
 
-sfL4 = Label(second_frame, text="Proof cycle",
+sfL4 = Label(calc_frame, text="Proof cycle",
             pady=2, padx=0).grid(row=3,column=0)
-sfL5 = Label(second_frame, text="MAX DEF.",
+sfL5 = Label(calc_frame, text="MAX DEF.",
             pady=0, padx=0).grid(row=4,column=0)
-sfL6 = Label(second_frame, text="RESIDUAL DEF.",
+sfL6 = Label(calc_frame, text="RESIDUAL DEF.",
             pady=0, padx=0).grid(row=5,column=0)
-sfL7 = Label(second_frame, text="RECOVERY",
+sfL7 = Label(calc_frame, text="RECOVERY",
             pady=0, padx=0).grid(row=6,column=0)
-sfL8 = Label(second_frame, text="RECOVERY %",
+sfL8 = Label(calc_frame, text="RECOVERY %",
             pady=0, padx=0).grid(row=7,column=0)
 for i in range(5):
-    Entry(second_frame).grid(row=4, column=1+i)
-    Entry(second_frame).grid(row=2, column=1+i)
+    Entry(calc_frame).grid(row=4, column=1+i)
+    Entry(calc_frame).grid(row=2, column=1+i)
 
 
 #height  = 10
